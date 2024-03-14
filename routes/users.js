19d5566b32e0495dd2174da25f2b6d12a1f8313e
Sarkis <sarkis_strelok@mail.ru>
2024-03-14 17:48:18 +0300

    первые настройки

---
+++ routes/users.js
@@ -0,0 +1,12 @@
+const { login, register, current } = require('../controllers/users');
+const express = require('express');
+const router = express.Router();
+
+// /api/user/login
+router.post('/login', login);
+// /api/user/register
+router.post('/register', register);
+// /api/user/current
+router.get('/current', current);
+
+module.exports = router;
