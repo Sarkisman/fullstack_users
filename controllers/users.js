44f8a7e54f8e37843131fd9c3b4721ad18a23675
Sarkis <sarkis_strelok@mail.ru>
2024-03-14 23:58:16 +0300

    прописаны ручки на регистрацию и авторизацию

--- controllers/users.js
+++ controllers/users.js
@@ -1,13 +1,15 @@
 const { prisma } = require('../prisma/prisma-client');
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 /** 
 @route POST /api/user/login
+@desc Логин
+@access Public
 */
-
 const login = async (req, res) => {
   const { mail, password } = req.body;
-  if (!mail && !password) {
+  if (!mail || !password) {
     return res
       .status(400)
       .json({ message: 'Пожалуйста заполните обязательные поля' });
@@ -17,16 +19,58 @@ const login = async (req, res) => {
       email,
     },
   });
-  const isPasswordCorrect = user && (await bcrypt.compare(password, user.password))
+  const isPasswordCorrect =
+    user && (await bcrypt.compare(password, user.password));
 
-  if(user && isPasswordCorrect) {
-    
+  if (user && isPasswordCorrect) {
+    res.status(200).json({ id: user.id, email: user.email, name: user.name });
+  } else {
+    return res.status(400).json({ massage: 'Неверно введён логин или пароль' });
   }
 };
 
+/** 
+@route POST /api/user/register
+@desc Регистрация
+@access Public
+*/
 const register = async (req, res) => {
-  res.send('register');
+  const { email, password, name } = req.body;
+
+  if (!email || !password || !name) {
+    return res
+      .status(400)
+      .json({ massage: 'Пожалуйста, заполните обязательные поля' });
+  }
+
+  const registeredUser = await prisma.user.findFirst({ where: { email } });
+
+  if (registeredUser) {
+    return res
+      .status(400)
+      .json({ message: 'Пользователь с таким Email уже существует' });
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const user = await prisma.user.create({
+    data: { email, name, password: hashedPassword },
+  });
+  const secret = process.env.JWT_SECRET;
+
+  if (user && secret) {
+    res.status(201).json({
+      id: user.id,
+      email: user.email,
+      name: user.name,
+      token: jwt.sign({ id: user.id }, secret, { expiresIn: '30d' }),
+    });
+  } else {
+    return res.status(400).json({ massage: 'Не удалось создать пользователя' });
+  }
 };
+
 const current = async (req, res) => {
   res.send('current');
 };
