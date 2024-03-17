ddb4d8c5af980f42c095fe4c55522b890ec20a6b
Sarkis <sarkis_strelok@mail.ru>
2024-03-17 15:17:26 +0300

    Добавлено фронтовое приложение на реакт-редакс с тайпскрипт

---
+++ client/src/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { store } from './app/store';
+import reportWebVitals from './reportWebVitals';
+import './index.css';
+import { Paths } from './paths';
+
+const router = createBrowserRouter([
+  { path: Paths.login, element: <h1>Login</h1> },
+  { path: Paths.register, element: <h1>Register</h1> },
+]);
+const container = document.getElementById('root')!;
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider>
+
+      </RouterProvider>
+    </Provider>
+  </React.StrictMode>
+);
+
+reportWebVitals();
